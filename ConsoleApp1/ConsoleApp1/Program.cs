956f25ac59a32a24288a3ca79c6b907f4f655c68
Sarimon Teeraratbongkoj (Prae) <sarimon.t@geotalent.co.th>
2018-03-05 13:10:01 +0700

    add project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? lat = 13.6763482;
+            double? lon = 100.7362492;
+
+            
+            Console.WriteLine($"{lat.Value.ToString("0.0000")}, {lon.Value.ToString("0.0000")}");
+            Console.ReadLine();
+        }
+
+    }
+}
